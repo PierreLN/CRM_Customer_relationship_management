ba25f3818fde3851746de98db1cf67e31ff0e718
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-29 14:34:52 -0400

    record add

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -55,4 +55,31 @@ def customer_record(request, pk):
         return render(request, 'record.html', {"customer_record": customer_record})
     else:
         messages.success(request, "You must be login to view that page")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+
+        delete_record = Record.objects.get(id=pk)
+        delete_record.delete()
+        messages.success(request, "Recored deleted")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be login to view that page")
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, 'Record Added')
+                return redirect('home')
+        
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, 'You must be login...')
         return redirect('home')
