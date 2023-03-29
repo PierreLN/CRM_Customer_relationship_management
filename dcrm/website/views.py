26039dc04617ae2e689cf2a2319ce818af3e6390
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-29 14:56:49 -0400

    update up

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -83,3 +83,17 @@ def add_record(request):
     else:
         messages.success(request, 'You must be login...')
         return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:            
+        update_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=update_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Record Updated')
+            return redirect('home')
+        return render(request, 'update_record.html', {"form": form})
+    else:
+        messages.success(request, 'You must be login...')        
+        return redirect('home')
